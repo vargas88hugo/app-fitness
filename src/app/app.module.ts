0ade12083b9c0da66498a8ce8c61069fbe8452d8
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-10 22:17:17 -0500

    [Feature] Refactored an implemented Loding Lazily for training module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,16 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { StoreModule } from '@ngrx/store';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
-import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { environment } from '../environments/environment'
@@ -30,14 +25,9 @@ import { reducers } from './app.reducer';
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    PastTrainingComponent,
-    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +41,8 @@ import { reducers } from './app.reducer';
     AngularFirestoreModule,
     AngularFireAnalyticsModule,
     AuthModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    AngularFirestoreModule
   ],
   providers: [
     AuthService,
@@ -59,6 +50,5 @@ import { reducers } from './app.reducer';
     UIService
   ],
   bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
 })
 export class AppModule { }
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { TrainingComponent } from './training.component';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingComponent } from './past-training/past-training.component';
 import { StopTrainingComponent } from './current-training/stop-training.component';
-import { MaterialModule } from '../material.module';
 import { SharedModule } from '../shared/shared.module';
+import { TrainingRoutingModule } from './training-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,11 +14,15 @@ import { SharedModule } from '../shared/shared.module';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
+    StopTrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
     StopTrainingComponent
   ],
   imports: [
     SharedModule,
-    AngularFirestoreModule
+    TrainingRoutingModule
   ],
   entryComponents: [StopTrainingComponent]
 })
