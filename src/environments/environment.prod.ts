29b4d29785712abcbce306864bea38e04377bc83
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-11 13:04:45 -0500

    [Feature] Improved style of training components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB-qxgkmPymH274wNK4i5vMGPu2ZJ5ZmF4",
+    authDomain: "ng-fitness-tracker-fcd5c.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-fcd5c.firebaseio.com",
+    projectId: "ng-fitness-tracker-fcd5c",
+    storageBucket: "ng-fitness-tracker-fcd5c.appspot.com",
+    messagingSenderId: "545500007656",
+    appId: "1:545500007656:web:ce7f977bfc3c5bfbf48d5d",
+    measurementId: "G-6M7XG9FE9F"
+  }
 };
